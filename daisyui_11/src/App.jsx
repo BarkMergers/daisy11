b699bd91643d3a86a2449489034511ccc48c7f5d
Mark Burgess <mburgess1983@gmail.com>
2025-07-21 17:41:27 +0100

    first

---
+++ daisyui_11/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+function App() {
+    return (
+        <>
+            <div className="m-2" data-theme="dark">
+                <button class="btn btn-primary">Dark Themed Button</button>
+            </div>
+            <div className="m-2" data-theme="light">
+                <button class="btn btn-primary">Light Themed Button</button>
+            </div>
+        </>
+    )
+}
+export default App
