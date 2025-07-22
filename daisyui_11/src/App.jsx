a249bb149ebd207e7d851f93947b211a5d9ff804
Mark Burgess <mburgess1983@gmail.com>
2025-07-22 09:23:03 +0100

    add rating

--- daisyui_11/src/App.jsx
+++ daisyui_11/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import NavBar from './navbar/NavBar'
+import Rating from './rating/Rating'
+import { useState } from 'react'
 
 function App() {
+
+    const [value, setValue] = useState(4);
+
     return (
         <>
             <NavBar title="MyTest"></NavBar>
-
 
             <div className="m-2" data-theme="dark">
                 <button class="btn btn-primary">Dark Themed Button</button>
@@ -13,6 +17,11 @@ function App() {
             <div className="m-2" data-theme="light">
                 <button class="btn btn-primary">Light Themed Button</button>
             </div>
+
+            <Rating stars={7} value={value} onChange={(v) => setValue(v) } ></Rating>
+
+            <div>{value}</div>
+
         </>
     )
 }
---
+++ daisyui_11/src/rating/Rating.jsx
@@ -0,0 +1,41 @@
+import React from 'React';
+import './Rating.css';
+
+function Rating({value, onChange, stars}) {
+
+    const updateValue = function (value) {
+
+        if (typeof onChange === 'function')
+            onChange(value);
+    }
+
+    stars = stars || 5;
+    value = value || Math.ceil(stars / 2); 
+    const starList = Array.from(Array(stars).keys())
+
+    //updateValue(value);
+
+
+    return (
+    
+        <div className="rating">
+            {
+                starList.map((i) => {
+                    return <input type="radio" onClick={() => updateValue(i + 1)} name={"rating-1"} className="mask mask-star" defaultChecked={i + 1 == value ? true : false}  aria-label={(i + 1) + " star"}  />
+                })
+            }
+            
+        </div>
+    );
+
+
+}
+
+export default Rating;
+
+
+//<input type="radio" onClick={() => updateValue(1)} name="rating-1" className="mask mask-star" aria-label="1 star" />
+//            <input type="radio" onChange={() => updateValue(2) } name="rating-1" className="mask mask-star" aria-label="2 star" defaultChecked />
+//            <input type="radio" onChange={() => updateValue(3) } name="rating-1" className="mask mask-star" aria-label="3 star" />
+//            <input type="radio" onChange={() => updateValue(4) } name="rating-1" className="mask mask-star" aria-label="4 star" />
+//            <input type="radio" onChange={() => updateValue(5) } name="rating-1" className="mask mask-star" aria-label="5 star" />
