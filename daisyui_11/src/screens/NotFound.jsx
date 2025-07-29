712ddc7527ff7a9f0e2d7318d363f8c30fd1d8c8
Mark Burgess <mburgess1983@gmail.com>
2025-07-29 16:21:37 +0100

    dd

--- daisyui_11/src/screens/NotFound.jsx
+++ daisyui_11/src/screens/NotFound.jsx
@@ -2,22 +2,29 @@ import { React } from 'react'
 import { useContext } from "react";
 import { UserContext } from '../App'
 
+
 export default function NotFound() {
 
     const globalData = useContext(UserContext);
 
+    const imageStyle = {
+        height: "250px",
+        width: "250px",
+        margin: "auto",
+        "mask-image": "url(./404.png)",
+        maskSize: "250px 250px",
+        background: "linear-gradient(red, blue)"
+    }
+
     return (
         <>
             <div>
-                Page was not found!!!
+                <div onClick={globalData.SetSpinnerVisible} style={imageStyle} ></div>
+                <div style={{ padding: "20px" }}>Page was not found</div>
             </div>
 
-            <div>
-                <button onClick={ globalData.SetSpinnerVisible }>Click me</button>
-            </div>
+
 
         </>
     );
-
-
 }
