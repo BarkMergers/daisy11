79c22440f1b36a2263bd5bcb775e168c497cfabd
Mark Burgess <mburgess1983@gmail.com>
2025-07-29 11:54:35 +0100

    ahh login url

--- daisyui_11/src/authConfig.js
+++ daisyui_11/src/authConfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
     auth: {
         clientId: "5298233e-f1f9-4b83-871f-72ca0b090f13",
         authority: "https://login.microsoftonline.com/6d52e229-7b09-4772-885e-edd85950a304",
-        redirectUri: "http://localhost:59414/",
+        redirectUri: "https://nice-beach-0b426541e.1.azurestaticapps.net/",
     },
     cache: {
         cacheLocation: "localStorage",
