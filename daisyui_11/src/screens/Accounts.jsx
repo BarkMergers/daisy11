e22b6127a755ddbaf45d6b3b1f1f8f556345f305
Mark Burgess <mburgess1983@gmail.com>
2025-07-30 13:15:08 +0100

    paged data

--- daisyui_11/src/screens/Accounts.jsx
+++ daisyui_11/src/screens/Accounts.jsx
@@ -30,8 +30,8 @@ export default function Accounts() {
 
         newPageIndex = newPageIndex || 0;
 
-        const url = `http://localhost:7039/api/GetCustomer/${newPageIndex}/5`;
-        //const url = `https://daisy11functions20250722145544.azurewebsites.net/api/GetCustomer/${newPageIndex}/5`;
+        //const url = `http://localhost:7039/api/GetCustomer/${newPageIndex}/5`;
+        const url = `https://daisy11functions20250722145544.azurewebsites.net/api/GetCustomer/${newPageIndex}/5`;
 
         const response = await fetch(url);
         const data = await response.json();
