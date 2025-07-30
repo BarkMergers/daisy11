b23b686b1f71bd7f542324e53e079db446652a04
Mark Burgess <mburgess1983@gmail.com>
2025-07-30 16:31:44 +0100

    styling issues

--- daisyui_11/src/screens/Home.jsx
+++ daisyui_11/src/screens/Home.jsx
@@ -1,4 +1,4 @@
-
+﻿
 
 import { React } from 'react'
 import Rating from '../rating/Rating';
@@ -7,19 +7,13 @@ export default function Home({ accounts }) {
 
 
     return (
-    
         <>
-
             {accounts.length == 0 ?
-
-                <>
+                <div className="m-auto">
                     Welcome to the testbed - Please log in with your corporate email (It should already exist in Azure Entra!)
-                </>
-
+                </div>
                 :
-
-                <>
-
+                <div className="m-auto text-center">
                     <div>
                         You are now logged in! Hover over your username to see your role. New features will be added here shortly.
                     </div>
@@ -28,18 +22,8 @@ export default function Home({ accounts }) {
                         <div>Rate it!</div>
                         <Rating stars={5}></Rating>
                     </div>
-
-
-
-                </>
-
-
+                </div>
             }
-
         </>
-
-
     );
-
-
 }
--- daisyui_11/src/spinnerLoader/SpinnerLoader.jsx
+++ daisyui_11/src/spinnerLoader/SpinnerLoader.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react'
+﻿import { useEffect, useRef } from 'react'
 
 export default function SpinnerLoader() {
     const modalRef = useRef(null);
@@ -19,7 +19,7 @@ export default function SpinnerLoader() {
     return (
         <dialog ref={modalRef} id={id} className="modal" onCancel={handleESC}>
             <form method="dialog" className="modal-box">
-                <h3 className="text-lg font-bold">Processing</h3>
+                <h3 className="text-center text-lg font-bold">Processing</h3>
                 <img style={{ "height": "150px", "width": "150px", "margin": "auto" }} src="./fade-stagger-squares.svg"></img>
             </form>
         </dialog>
