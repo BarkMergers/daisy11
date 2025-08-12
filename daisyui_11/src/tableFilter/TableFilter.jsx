e19d1d27d1440e1cf3d51cc93bd91bde1babb018
Mark Burgess <mburgess1983@gmail.com>
2025-08-12 10:10:10 +0100

    filters

--- daisyui_11/src/tableFilter/TableFilter.jsx
+++ daisyui_11/src/tableFilter/TableFilter.jsx
@@ -1,26 +1,33 @@
 ﻿import Input from '../input/Input';
 import Select from '../select/Select';
 import './TableFilter.css';
+import { FaSearch } from 'react-icons/fa';
 
 export default function TableFilter({ openEditor, filterData, applyFilter }) {
     return (
 
         <div className="my-2 rounded-xl bg-white py-1">
-            <select className="select mx-1 h-8 min-h-0"><option>Show all</option><option>Show active</option></select>
+            <select className="select mx-1 h-8 min-h-0 align-middle"><option>Show all</option><option>Show active</option></select>
 
-            <input className="input mx-1 h-8 min-h-0" placeholder="Search"></input>
 
-            <select onChange={(e) => applyFilter(e.target, "status")} className="select mx-1 h-8 min-h-0"><option value="">Status</option>
+
+            <div className="input search-container">
+                <FaSearch className="search-icon" />
+                <input onChange={(e) => applyFilter(e.target, "text")} type="text" placeholder="Search vehicle..." className="search-input" style={{ border:"none", background:"none" }} />
+            </div>
+
+
+            <select onChange={(e) => applyFilter(e.target, "status")} className="select align-middle mx-1 h-8 min-h-0"><option value="">Status...</option>
                 {filterData?.statusList != null && filterData.statusList.map((i) => <option key={i}>{i}</option>)}
             </select>
 
             {/*<select className="select mx-1 h-8 min-h-0"><option>Description</option></select>*/}
 
-            <select onChange={(e) => applyFilter(e.target, "issuer")} className="select mx-1 h-8 min-h-0"><option value="">Issuer</option>
+            <select onChange={(e) => applyFilter(e.target, "issuer")} className="select align-middle mx-1 h-8 min-h-0"><option value="">Issuer...</option>
                 {filterData?.issuerList != null && filterData.issuerList.map((i) => <option key={i}>{i}</option>)}
             </select>
 
-            <select onChange={(e) => applyFilter(e.target, "fineoperator")} className="select mx-1 h-8 min-h-0"><option value="">Operators</option>
+            <select onChange={(e) => applyFilter(e.target, "fineoperator")} className="select align-middle mx-1 h-8 min-h-0"><option value="">Operators...</option>
                 {filterData?.operatorList != null && filterData.operatorList.map((i) => <option key={i}>{i}</option>)}
             </select>
 
